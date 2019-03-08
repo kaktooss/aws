dc23f37eb7ab7815fe77c46a8827233d93a1eb4f
PJ <paul.jakimov@gmail.com>
2019-03-08 12:49:09 +0100

    Group images with push

--- build.sh
+++ build.sh
@@ -3,10 +3,9 @@
 export VERSION=`date +%Y%m%d-%H%M%S -u`
 
 docker build  -f ubuntu-aws.dockerfile       -t kaktooss/aws       -t kaktooss/aws:${VERSION} .
-docker build  -f ubuntu-aws-java.dockerfile  -t kaktooss/aws-java  -t kaktooss/aws-java:${VERSION} .
-
 docker push kaktooss/aws:${VERSION}
 docker push kaktooss/aws:latest
 
+docker build  -f ubuntu-aws-java.dockerfile  -t kaktooss/aws-java  -t kaktooss/aws-java:${VERSION} .
 docker push kaktooss/aws-java:${VERSION}
 docker push kaktooss/aws-java:latest
